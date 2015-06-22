68c7451f6deaa2d689555df37091cac85c911084
William Casarin <bill@casarin.me>
2015-06-22 09:53:20 -0700

    update packages

--- configuration.nix
+++ configuration.nix
@@ -27,31 +27,42 @@
   networking.hostId = "900eef22";
   # networking.wireless.enable = true;  # Enables wireless.
   hardware.bluetooth.enable = true;
+  hardware.pulseaudio.enable = true;
 
   environment.systemPackages = with pkgs; [
+    apvlv
+    bc
     chromium
-    dmenu
-    ipafont                            # japanese fonts
-    emacs
     compton
-    notmuch
-    mbsync
-    redshift
-    hsetroot
+    dmenu
+    emacs
     file
+    git-extras
     gitFull
-    haskellngPackages.cabal2nix
-    haskellngPackages.cabal-install
-    haskellngPackages.ghc
-    haskellngPackages.hlint
+    haskellPackages.ShellCheck
+    haskellPackages.cabal-install
+    haskellPackages.cabal2nix
+    haskellPackages.ghc
+    haskellPackages.hlint
+    hsetroot
     htop
+    ipafont
+    lsof
     nix-repl
+    notmuch
+    popcorntime
+    redshift
+    rsync
     rxvt_unicode
     scrot
     silver-searcher
-    vim
-    wget
+    steam
+    subversion
     unzip
+    vim
+    vlc
+    wget
+    xclip
     xdg_utils
     xlibs.xev
     xlibs.xset
@@ -62,6 +73,23 @@
     chromium.enablePepperFlash = true;
     chromium.enablePepperPDF = true;
   };
+
+ #services.btsync = {
+ #  # disable for now until I fix this
+ #  enable = false;
+ #  deviceName = "monad";
+ #  httpListenPort = 9902;
+ #  storagePath = "/home/jb55/btsync";
+ #  sharedFolders = [{
+ #    secret         = "AFNEZRTN4VI2MKMSWKINZDHSGLOMVQJQU";
+ #    directory      = "/home/jb55/src";
+ #    useRelayServer = true;
+ #    useTracker     = true;
+ #    useDHT         = false;
+ #    searchLAN      = true;
+ #    useSyncTrash   = true;
+ #  }];
+ #};
 
   services.xserver = {
     enable = true;
@@ -116,7 +144,20 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+  };
+
+  # bittorrent web ui
+ #services.transmission = {
+ #  enable = false;
+ #  settings = {
+ #    download-dir = "/home/jb55/torrents";
+ #    incomplete-dir-enabled = false;
+ #    rpc-whitelist = "127.0.0.1,192.168.*.*";
+ #  };
+ #};
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
@@ -127,29 +168,29 @@
   virtualisation.docker.enable = true;
 
   programs.zsh.enable = true;
-  programs.zsh.interactiveShellInit =
-    ''
-      # Taken from <nixos/modules/programs/bash/command-not-found.nix>
-      # and adapted to zsh (i.e. changed name from 'handle' to
-      # 'handler').
+ #programs.zsh.interactiveShellInit =
+ #  ''
+ #    # Taken from <nixos/modules/programs/bash/command-not-found.nix>
+ #    # and adapted to zsh (i.e. changed name from 'handle' to
+ #    # 'handler').
 
-      # This function is called whenever a command is not found.
-      command_not_found_handler() {
-        local p=/run/current-system/sw/bin/command-not-found
-        if [ -x $p -a -f /nix/var/nix/profiles/per-user/root/channels/nixos/programs.sqlite ]; then
-          # Run the helper program.
-          $p "$1"
-          # Retry the command if we just installed it.
-          if [ $? = 126 ]; then
-            "$@"
-          else
-            return 127
-          fi
-        else
-          echo "$1: command not found" >&2
-          return 127
-        fi
-      }
-    '';
+ #    # This function is called whenever a command is not found.
+ #    command_not_found_handler() {
+ #      local p=/run/current-system/sw/bin/command-not-found
+ #      if [ -x $p -a -f /nix/var/nix/profiles/per-user/root/channels/nixos/programs.sqlite ]; then
+ #        # Run the helper program.
+ #        $p "$1"
+ #        # Retry the command if we just installed it.
+ #        if [ $? = 126 ]; then
+ #          "$@"
+ #        else
+ #          return 127
+ #        fi
+ #      else
+ #        echo "$1: command not found" >&2
+ #        return 127
+ #      fi
+ #    }
+ #  '';
 }
 
