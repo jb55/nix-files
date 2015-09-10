d1bad0da1897670f58832161832ee67f86b2a8a4
William Casarin <bill@casarin.me>
2015-09-10 03:41:08 -0700

    Add some useful env bundles to .nixpkgs/config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,12 +1,19 @@
-{
+{ allowUnfree = true;
+  allowUnfreeRedistributable = true;
+  obs-studio.pulseaudio = true;
+  steam.java = true;
   allowBroken = false;
-  allowUnfree = true;
+
+  chromium = {
+    enablePepperFlash = true;
+    enablePepperPDF = true;
+  };
 
   packageOverrides = super: let self = super.pkgs; in
   {
     haskellDevToolsEnv = self.buildEnv {
       name = "haskellDevTools";
-      paths = with self.haskell.packages.ghc784; [
+      paths = with self.haskellPackages; [
         cabal2nix
         hindent
         hlint
@@ -20,20 +27,31 @@
       ];
     };
 
-#   haskellPackages = super.haskellPackages.override {
-#     overrides = self: super: {
-#       hierarchy = self.callPackage ~/dev/haskell/hierarchy {};
-#     };
-#   };
+    syntaxCheckersEnv = self.buildEnv {
+      name = "syntaxCheckers";
+      paths = with self; [
+        haskellPackages.ShellCheck
+      ];
+    };
 
-#   haskell = super.haskell // {
-#     packages = super.haskell.packages // {
-#       ghc784 = super.haskell.packages.ghc784.override {
-#         overrides = self: super: {
-#           hierarchy = self.callPackage ~/dev/haskell/hierarchy {};
-#         };
-#       };
-#     };
-#   };
+    machineLearningToolsEnv = self.buildEnv {
+      name = "machineLearningTools";
+      paths = with self; [
+        caffe
+      ];
+    }
+
+    xlsx2csv = super.pythonPackages.buildPythonPackage rec {
+      name = "xlsx2csv-0.7.2";
+      src = self.pkgs.fetchurl {
+        url = "https://pypi.python.org/packages/source/x/xlsx2csv/${name}.tar.gz";
+        md5 = "eea39d8ab08ff4503bb145171d0a46f6";
+      };
+      meta = {
+        homepage = https://github.com/bitprophet/alabaster;
+        description = "xlsx2csv converter";
+        license = self.stdenv.lib.licenses.bsd3;
+      };
+    };
   };
 }
