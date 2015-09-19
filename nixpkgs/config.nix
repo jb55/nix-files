ea268b10c5c3e242b51a019a98141f26c9cb1d5d
William Casarin <bill@casarin.me>
2015-09-18 18:07:44 -0700

    haskellEnv with my packages

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,23 +1,26 @@
-{ allowUnfree = true;
+{ 
+  allowUnfree = true;
   allowUnfreeRedistributable = true;
   obs-studio.pulseaudio = true;
   steam.java = true;
   allowBroken = false;
+  zathura.useMupdf = true;
 
   chromium = {
     enablePepperFlash = true;
     enablePepperPDF = true;
   };
 
-  packageOverrides = super: let self = super.pkgs; in
-  {
-    haskellDevToolsEnv = self.buildEnv {
-      name = "haskellDevTools";
-      paths = with self.haskellPackages; [
+  packageOverrides = super: let pkgs = super.pkgs; in
+  rec {
+    haskellEnv = pkgs.buildEnv {
+      paths = with pkgs.haskellPackages; [
+        (ghcWithHoogle myHaskellPackages)
         cabal2nix
         hindent
         hlint
-        ghc-mod
+        #ghc-mod
+        #hdevtools
         ghc-core
         structured-haskell-mode
         hasktags
@@ -40,6 +43,174 @@
         caffe
       ];
     };
+
+    myHaskellPackages = hp: with hp; [
+      # fixplate
+      # orgmode-parse
+      Boolean
+      # CC-delcont
+      HTTP
+      HUnit
+      MissingH
+      QuickCheck
+      aeson
+      # arithmoi
+      async
+      attoparsec
+      bifunctors
+      blaze-builder
+      blaze-builder-conduit
+      blaze-builder-enumerator
+      blaze-html
+      blaze-markup
+      blaze-textual
+      cased
+      cereal
+      comonad
+      comonad-transformers
+      # compdata
+      dlist
+      dlist-instances
+      doctest
+      # errors
+      exceptions
+      fingertree
+      foldl
+      folds
+      free
+      hamlet
+      hashable
+      hspec
+      hspec-expectations
+      html
+      http-client
+      http-date
+      http-types
+      io-memoize
+      language-c
+      language-javascript
+      lens
+      lens-action
+      lens-aeson
+      lens-datetime
+      lens-family
+      lens-family-core
+      lifted-async
+      lifted-base
+      linear
+      # linearscan
+      # linearscan-hoopl
+      list-extras
+      list-t
+      logict
+      # machines
+      mime-mail
+      mime-types
+      mmorph
+      monad-control
+      monad-coroutine
+      monad-loops
+      monad-par
+      monad-par-extras
+      monad-stm
+      monadloc
+      monoid-extras
+      network
+      newtype
+      numbers
+      optparse-applicative
+      # pandoc
+      parsec
+      parsers
+      pipes
+      pipes-attoparsec
+      pipes-binary
+      pipes-bytestring
+      pipes-concurrency
+      pipes-csv
+      pipes-mongodb
+      pipes-extras
+      pipes-group
+      pipes-http
+      pipes-network
+      pipes-parse
+      pipes-safe
+      pipes-shell
+      pipes-text
+      posix-paths
+      #postgresql-simple
+      pretty-show
+      profunctors
+      random
+      # recursion-schemes
+      reducers
+      reflection
+      regex-applicative
+      regex-base
+      regex-compat
+      regex-posix
+      regular
+      resourcet
+      retry
+      rex
+      SafeSemaphore
+      safe
+      sbv
+      scotty
+      semigroupoids
+      semigroups
+      shake
+      shakespeare
+      shelly
+      simple-reflect
+      # singletons
+      speculation
+      split
+      spoon
+      stm
+      stm-chans
+      stm-stats
+      streaming
+      streaming-bytestring
+      strict
+      stringsearch
+      strptime
+      syb
+      system-fileio
+      system-filepath
+      tagged
+      tar
+      tardis
+      tasty
+      tasty-hspec
+      tasty-hunit
+      tasty-quickcheck
+      tasty-smallcheck
+      temporary
+      text
+      text-format
+      # these
+      # thyme
+      time
+      # time-recurrence
+      # timeparsers
+      transformers
+      transformers-base
+      turtle
+      uniplate
+      # units
+      unix-compat
+      unordered-containers
+      uuid
+      vector
+      void
+      wai
+      warp
+      xhtml
+      yaml
+      zippers
+      zlib
+    ];
 
     xlsx2csv = super.pythonPackages.buildPythonPackage rec {
       name = "xlsx2csv-0.7.2";
