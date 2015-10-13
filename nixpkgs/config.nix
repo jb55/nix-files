654a53d8c56917207480753c4ad96fd84b1060db
William Casarin <bill@casarin.me>
2015-10-13 13:04:36 -0700

    fix haskell tools env

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -57,9 +57,9 @@
       withHoogle = false;
     };
 
-    haskellToolsEnv = haskellEnvFun {
+    haskellToolsEnv = super.buildEnv {
       name = "haskellTools";
-      withPackages = false;
+      paths = haskellTools super.haskellPackages;
     };
 
     haskellEnvFun = { tools ? haskellTools, withHoogle ? false, withPackages ? true, compiler ? null, name }:
