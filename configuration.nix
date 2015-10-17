36caecf0379521c033cd4e61524a20449f5bf793
William Casarin <bill@casarin.me>
2015-10-16 21:56:55 -0700

    xautolock: call slock after 10 mins

--- configuration.nix
+++ configuration.nix
@@ -97,6 +97,7 @@ in {
     vim
     vlc
     wget
+    xautolock
     xbindkeys
     xclip
     xdg_utils
@@ -146,6 +147,7 @@ in {
         ${pkgs.hsetroot}/bin/hsetroot -solid '#1a2028'
         ${pkgs.xbindkeys}/bin/xbindkeys
         ${pkgs.feh}/bin/feh --bg-fill $HOME/etc/img/polygon1.png
+        ${pkgs.xautolock}/bin/xautolock -time 10 -locker slock &
       '';
 
       lightdm.enable = true;
