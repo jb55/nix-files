3007896c573db197f4ab165cbb51371f6fb6a7d8
William Casarin <bill@casarin.me>
2015-12-16 11:25:28 -0800

    add monstercat haskell packages to overrides

--- nixpkgs/haskell-overrides.nix
+++ nixpkgs/haskell-overrides.nix
@@ -1,5 +1,6 @@
 pkgs: self: super:
 let overrideCabal = pkgs.haskell.lib.overrideCabal;
+    monstercatPkgs = import /home/jb55/etc/monstercatpkgs { inherit pkgs; };
 in {
   streaming-wai = self.callPackage ~/src/haskell/streaming-wai {};
   pipes = overrideCabal super.pipes (drv: {
@@ -24,4 +25,4 @@ in {
       sha256 = "1r1dv7yaalxja06jxqi7rjcdkb72mb2prnk8crzqap0gkmbahqcd";
     };
   });
-}
+} // monstercatPkgs.haskellPackages
