f7851f77c65403c3426aff5d1b71b5dc5b86536f
William Casarin <bill@casarin.me>
2016-01-11 13:40:52 -0800

    xinitrc-refresh: to get around nixos issue
    An issue which happens during nixos-rebuild

--- nixpkgs/dotfiles.nix
+++ nixpkgs/dotfiles.nix
@@ -16,6 +16,12 @@ let
     url = "http://jb55.com/img/haskell-space.jpg";
     md5 = "04d86f9b50e42d46d566bded9a91ee2c";
   };
+  impureSessionCommands = ''
+    #!${pkgs.bash}/bin/bash
+    ${pkgs.xlibs.xmodmap}/bin/xmodmap ${dotfiles}/.Xmodmap
+    ${pkgs.xlibs.xset}/bin/xset r rate 200 50
+    ${pkgs.xlibs.xset}/bin/xset m 0 0
+  '';
   sessionCommands = ''
     #!${pkgs.bash}/bin/bash
     ${pkgs.feh}/bin/feh --bg-fill ${bgimg}
@@ -23,11 +29,9 @@ let
     ${pkgs.parcellite}/bin/parcellite &
     ${pkgs.xautolock}/bin/xautolock -time 10 -locker slock &
     ${pkgs.xbindkeys}/bin/xbindkeys -f ${dotfiles}/.xbindkeysrc
-    ${pkgs.xlibs.xmodmap}/bin/xmodmap ${dotfiles}/.Xmodmap
-    ${pkgs.xlibs.xset}/bin/xset r rate 200 50
-    ${pkgs.xlibs.xset}/bin/xset m 0 0
-   '' + "\n" + machineSessionCommands;
+   '' + "\n" + impureSessionCommands + "\n" + machineSessionCommands;
   xinitrc = writeScript "xinitrc" sessionCommands;
+  xinitrc-refresh = writeScript "xinitrc-refresh" impureSessionCommands;
 in stdenv.mkDerivation rec {
   name = "jb55-config-${version}";
   version = "git-2015-12-26";
@@ -39,6 +43,7 @@ in stdenv.mkDerivation rec {
     echo "user config at '$out'"
     ln -s "${dotfiles}" $out/dotfiles
     cp "${xinitrc}" $out/bin/xinitrc
+    cp "${xinitrc-refresh}" $out/bin/xinitrc-refresh
     ln -s $out/bin/xinitrc $out/.xinitrc
   '';
 }
