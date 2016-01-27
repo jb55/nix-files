1c6f055e5e44d8c3ddb18c578c4b765c3a058595
William Casarin <bill@casarin.me>
2016-01-26 19:05:35 -0800

    monad: fixes

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,17 +8,28 @@
     [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "xhci_hcd" "firewire_ohci" "usb_storage" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "xhci_pci" "firewire_ohci" "usbhid" "usb_storage" "sd_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/611fd236-706f-4637-be5c-b61b6b9b071f";
+    { device = "/dev/disk/by-uuid/62518649-0872-49e2-a269-34975e314c6a";
       fsType = "ext4";
     };
 
+  fileSystems."/sand" =
+    { device = "/dev/disk/by-uuid/2ee709b8-7e83-470f-91bc-d0b0ba59b945";
+      fsType = "ext4";
+    };
+
+  fileSystems."/home/jb55/.local/share/Steam/steamapps" =
+    { device = "/sand/data/SteamAppsLinux";
+      fsType = "none";
+      options = "bind";
+    };
+
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/7bec20f3-8c78-4af8-9aa1-29ae651e8a5c"; }
+    [ { device = "/dev/disk/by-uuid/d4e4ae51-9179-439d-925b-8df42dd1bfc5"; }
     ];
 
   nix.maxJobs = 8;
