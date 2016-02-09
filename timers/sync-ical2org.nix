4e1c3f3cb1b6dc1dc95d663deb11c4dca1ec86ab
William Casarin <bill@casarin.me>
2016-02-08 21:27:59 -0800

    sync all my calendars

--- timers/sync-ical2org.nix
+++ timers/sync-ical2org.nix
@@ -1,22 +1,41 @@
 home:
 { config, lib, pkgs, ... }:
-let calendar = (import ../private.nix).calendar;
+let calendars = (import ../private.nix).calendars;
+    calendarArgs = with pkgs.lib;
+      let xs = mapAttrsToList (n: v: "'" + n + "=" + v + "'") calendars;
+      in concatStringsSep " " xs;
 in {
   systemd.services.sync-ical2org = {
     description = "Sync gcal calendar to calendar.org";
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = pkgs.writeScript "ical2org-auto" ''
-        #!${pkgs.bash}/bin/bash
-        set -e
-        caldir="${home}/var/ical2org"
-        link="${calendar}"
-        mkdir -p "$caldir"
-        export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-        ${pkgs.curl}/bin/curl -L "$link" \
-          | ${pkgs.ical2org}/bin/ical2org > "$caldir/calendar.org"
-      '';
+      ExecStart = let script = pkgs.writeScript "ical2org-auto" ''
+        #!${pkgs.python32}/bin/python3
+        import os
+        import sys
+        from urllib.request import urlopen
+        import subprocess
+        caldir = "${home}/var/ical2org"
+        os.makedirs(caldir, exist_ok=True)
+        cat = lambda n: b"#+CATEGORY:    " + bytes(n, "utf-8")
+        for arg in sys.argv[1:]:
+          [name, link] = arg.split("=")
+          ical = urlopen(link).read()
+          fname = os.path.join(caldir, name + ".org")
+          org = open(fname, "wb")
+          proc = subprocess.Popen("${pkgs.ical2org}/bin/ical2org",
+                                  close_fds=True,
+                                  stdin=subprocess.PIPE,
+                                  stdout=subprocess.PIPE)
+          out, err = proc.communicate(ical)
+          org.write(out.replace(cat("google"), cat(name)))
+          org.close()
+      ''; in "${script} ${calendarArgs}";
+
     };
+    preStart = ''
+      export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    '';
     restartIfChanged = false;
     startAt = "*:0/10";
   };
