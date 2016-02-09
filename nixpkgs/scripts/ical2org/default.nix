672a96d89fe08fae086ca1e7f12783285df13c4a
William Casarin <bill@casarin.me>
2016-02-08 22:28:17 -0800

    ical2org: customize options

--- nixpkgs/scripts/ical2org/default.nix
+++ nixpkgs/scripts/ical2org/default.nix
@@ -17,7 +17,13 @@ stdenv.mkDerivation rec {
     cp $src $out/bin/ical2org
     chmod +x $out/bin/ical2org
     substituteInPlace $out/bin/ical2org \
-      --replace "/usr/bin/awk" "${gawk}/bin/gawk"
+      --replace "/usr/bin/awk" "${gawk}/bin/gawk" \
+      --replace "max_age = 7" "max_age = -1" \
+      --replace "condense = 0" "condense = 1" \
+      --replace "original = 1" "original = 0" \
+      --replace "preamble = 1" "preamble = 0" \
+      --replace 'author = "Eric S Fraga"' 'author = "William Casarin"' \
+      --replace 'emailaddress = "e.fraga@ucl.ac.uk"' 'emailaddress = "bill@casarin.me"'
   '';
 
   meta = with stdenv.lib; {
