e85178830bf2ca27a00b6a1b4cedf94ab4f8779f
William Casarin <bill@casarin.me>
2016-02-10 09:01:17 -0800

    networking: dont override previous settings

--- networking/default.nix
+++ networking/default.nix
@@ -1,14 +1,12 @@
 machine:
 { config, lib, pkgs, ... }:
 {
-  networking = {
-    hostName = machine;
-    extraHosts = ''
-      174.143.211.135 freenode.znc.jb55.com
-      172.24.160.85 phone
-      172.24.14.20 archer
-    '';
+  networking.hostName = machine;
+  networking.extraHosts = ''
+    174.143.211.135 freenode.znc.jb55.com
+    172.24.160.85 phone
+    172.24.14.20 archer
+  '';
 
-    firewall.allowPing = true;
-  };
+  networking.firewall.allowPing = true;
 }
