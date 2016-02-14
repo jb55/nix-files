491b93196bfbd78896440bd7422670ce1e46163c
William Casarin <bill@casarin.me>
2016-02-14 01:40:19 -0800

    charon: override jb55.com for now

--- networking/default.nix
+++ networking/default.nix
@@ -6,7 +6,7 @@ machine:
     174.143.211.135 freenode.znc.jb55.com
     172.24.160.85 phone
     172.24.14.20 archer
-    172.24.206.82 charon
+    172.24.206.82 charon jb55.com
   '';
 
   networking.firewall.allowPing = true;
