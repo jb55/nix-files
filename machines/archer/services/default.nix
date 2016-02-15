74977c946f27cd8048d37df28c1de146b9124f6b
William Casarin <bill@casarin.me>
2016-02-15 14:09:51 -0800

    archer: working massager service

--- machines/archer/services/default.nix
+++ machines/archer/services/default.nix
@@ -2,15 +2,20 @@
 {
   systemd.services.massager = {
     description = "Massager service";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "mongodb.target" ];
+
     serviceConfig = {
       Type = "simple";
 
-      wantedBy = [ "multi-user.target" ];
-      after = [ "mongodb.target" ];
-
+      WorkingDirectory = "${pkgs.haskellPackages.massager-service}/bin";
       ExecStart = "${pkgs.haskellPackages.massager-service}/bin/massager-service";
     };
     restartIfChanged = false;
+
+    preStart = ''
+      ${pkgs.mongodb}/bin/mongo massager --eval 'db.User.remove({ name: null })'
+    '';
   };
 
   systemd.services.massager.enable = true;
