1b8c1f6388ffb1d8b04bec87d52f2bee2b3cc5d0
William Casarin <bill@casarin.me>
2016-06-19 11:56:20 -0700

    sync up with work a bit

--- machines/archer/services/default.nix
+++ machines/archer/services/default.nix
@@ -18,5 +18,18 @@
     '';
   };
 
+  services.postgresql = {
+    dataDir = "/var/db/postgresql/9.5/";
+    enable = true;
+    authentication = pkgs.lib.mkForce ''
+      # type db  user address        method
+      local  all all                 trust
+      host   all all  10.243.0.0/16  trust
+    '';
+    extraConfig = ''
+      listen_addresses = '10.243.14.20'
+    '';
+  };
+
   systemd.services.massager.enable = true;
 }
