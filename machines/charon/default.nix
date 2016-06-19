6e3146f480145cfeff2d04bab3e8d3f4b04acf18
William Casarin <bill@casarin.me>
2016-06-19 12:01:47 -0700

    charon: postgresql

--- machines/charon/default.nix
+++ machines/charon/default.nix
@@ -5,4 +5,14 @@
     ./hardware
     ./nginx
   ];
+
+  services.postgresql = {
+    dataDir = "/var/db/postgresql/9.5/";
+    enable = true;
+    authentication = pkgs.lib.mkForce ''
+      # type db  user address        method
+      local  all all                 trust
+      host   all all  172.24.0.0/16  trust
+    '';
+  };
 }
