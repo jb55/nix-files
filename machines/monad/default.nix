3cd95f38be7a5582ce505bf12827e161b92823a2
William Casarin <bill@casarin.me>
2016-06-21 10:45:42 -0700

    monad: simplify further

--- machines/monad/default.nix
+++ machines/monad/default.nix
@@ -2,10 +2,12 @@
 {
   imports = [
     ./hardware
-    ./environment
   ];
 
   networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
   networking.firewall.allowedTCPPorts = [ 8999 22 143 80 5000 ];
   networking.firewall.allowedUDPPorts = [ 11155 ];
+
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "jb55" ];
 }
