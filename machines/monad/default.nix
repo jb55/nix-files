34bc2aac18cb8f1a1a7ba1cae90340d5988416ce
William Casarin <bill@casarin.me>
2016-06-21 10:45:09 -0700

    monad: simplify

--- machines/monad/default.nix
+++ machines/monad/default.nix
@@ -1,10 +1,11 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
-    ./networking
     ./hardware
     ./environment
   ];
 
   networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+  networking.firewall.allowedTCPPorts = [ 8999 22 143 80 5000 ];
+  networking.firewall.allowedUDPPorts = [ 11155 ];
 }
