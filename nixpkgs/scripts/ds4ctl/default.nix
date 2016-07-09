ce95b01fc92488cb39ef9fb05a74f0042da68885
William Casarin <bill@casarin.me>
2016-07-09 12:27:58 -0700

    ds4ctl: update to latest

--- nixpkgs/scripts/ds4ctl/default.nix
+++ nixpkgs/scripts/ds4ctl/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "ds4ctl-${version}";
-  version = "0.1";
+  version = "0.5";
 
   src = fetchFromGitHub {
     owner = "jb55";
     repo = "ds4ctl";
     rev = version;
-    sha256 = "0fy629w1yp9ww696nnr2dgg7qvpzildpwbxw71krbmxgahwyckr5";
+    sha256 = "0pq2l0fsksfvxjb2jvv7984pjrw1537wajvr5csl7a1ldvng0llz";
   };
 
   makeFlags = "PREFIX=$(out)";
