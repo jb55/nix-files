35aea96dc4df29372571b0ed49fe3f3ae26651e0
William Casarin <bill@casarin.me>
2016-07-09 14:49:35 -0700

    udev: add vive

--- machines/monad/default.nix
+++ machines/monad/default.nix
@@ -12,9 +12,14 @@
   users.extraGroups.vboxusers.members = [ "jb55" ];
 
   services.udev.extraRules = ''
+    # ds4
     KERNEL=="uinput", MODE="0666"
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", KERNELS=="0005:054C:05C4.*", MODE="0666"
+
+    # vive
+    KERNEL=="hidraw*", ATTRS{idVendor}=="0bb4", MODE="0666"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
   '';
 
   systemd.services.ds4ctl = {
