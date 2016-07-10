e32c2f6cb0e6c9c97407321654a42fccaed8966d
William Casarin <bill@casarin.me>
2016-07-09 19:14:19 -0700

    vive proper

--- machines/monad/default.nix
+++ machines/monad/default.nix
@@ -17,9 +17,17 @@
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", KERNELS=="0005:054C:05C4.*", MODE="0666"
 
-    # vive
-    KERNEL=="hidraw*", ATTRS{idVendor}=="0bb4", MODE="0666"
-    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", MODE="0666"
+    # vive hmd
+    KERNEL=="hidraw*", ATTRS{idVendor}=="0bb4", ATTRS{idProduct}=="2c87", MODE="0666"
+
+    # vive lighthouse
+    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2000", MODE="0666"
+
+    # vive controller
+    KERNEL=="hidraw*", ATTRS{idVendor}=="28de", ATTRS{idProduct}=="2101", MODE="0666"
+
+    # vive audio
+    KERNEL=="hidraw*", ATTRS{idVendor}=="0d8c", ATTRS{idProduct}=="0012", MODE="0666"
   '';
 
   systemd.services.ds4ctl = {
