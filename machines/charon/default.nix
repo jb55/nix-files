a19b81dc111e5fec7dcdd5a19282fe58af98c7fe
William Casarin <bill@casarin.me>
2016-07-16 15:50:44 -0700

    dnsmasq ad-blocking server

--- machines/charon/default.nix
+++ machines/charon/default.nix
@@ -1,4 +1,13 @@
 { config, lib, pkgs, ... }:
+let adblock-hosts = pkgs.fetchurl {
+      url    = "https://jb55.com/s/ad-sources.txt";
+      sha256 = "d9e6ae17ecc41eb7021c0552548a1c8da97efbb61e3a750fb023674d01d81134";
+    };
+    dnsmasq-adblock = pkgs.fetchurl {
+      url = "https://jb55.com/s/dnsmasq-ad-sources.txt";
+      sha256 = "f1af2fd53abaa15d3fd94a3318f45434a7a07dcf3e09d626b81e302ac254afb6";
+    };
+in
 {
   imports = [
     ./networking
@@ -54,6 +63,14 @@
   systemd.services.weechat.enable = true;
   systemd.services.postgrest.enable = true;
 
-  networking.firewall.allowedTCPPorts = [ 22 443 80 5432 ];
+  services.dnsmasq.enable = true;
+  services.dnsmasq.servers = ["8.8.8.8" "8.8.4.4"];
+  services.dnsmasq.extraConfig = ''
+    addn-hosts=${adblock-hosts}
+    conf-file=${dnsmasq-adblock}
+  '';
+
+  networking.firewall.allowedTCPPorts = [ 22 443 80 5432 53 ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
   networking.firewall.trustedInterfaces = ["zt0"];
 }
