fc7f27be2a53c8c11cb9613a1d1b7be53be57e9f
William Casarin <bill@casarin.me>
2016-07-30 21:53:45 -0700

    Revert "refactor session commands"
    This reverts commit c2d8b6aa3d64b5ce4738abbbfd616ab04c5d2b39.

---
+++ nixpkgs/dotfiles.nix
@@ -0,0 +1,45 @@
+{ pkgs
+, fetchFromGitHub
+, fetchurl
+, stdenv
+, writeScript
+, machineSessionCommands ? ""
+}:
+let
+  dotfiles = pkgs.jb55-dotfiles;
+  bgimg = fetchurl {
+    url = "http://jb55.com/img/haskell-space.jpg";
+    md5 = "04d86f9b50e42d46d566bded9a91ee2c";
+  };
+  impureSessionCommands = ''
+    #!${pkgs.bash}/bin/bash
+    ${pkgs.xlibs.xmodmap}/bin/xmodmap ${dotfiles}/.Xmodmap
+    ${pkgs.xlibs.xset}/bin/xset r rate 200 50
+  '' + "\n" + machineSessionCommands;
+  sessionCommands = ''
+    #!${pkgs.bash}/bin/bash
+    ${pkgs.feh}/bin/feh --bg-fill ${bgimg}
+    ${pkgs.haskellPackages.taffybar}/bin/taffybar &
+    ${pkgs.clipit}/bin/clipit &
+    ${pkgs.volumeicon}/bin/volumeicon &
+    ${pkgs.xautolock}/bin/xautolock -time 10 -locker slock &
+    ${pkgs.xbindkeys}/bin/xbindkeys -f ${dotfiles}/.xbindkeysrc
+    ${pkgs.xlibs.xsetroot}/bin/xsetroot -cursor_name left_ptr
+  '' + "\n" + impureSessionCommands;
+  xinitrc = writeScript "xinitrc" sessionCommands;
+  xinitrc-refresh = writeScript "xinitrc-refresh" impureSessionCommands;
+in stdenv.mkDerivation rec {
+  name = "jb55-config-${version}";
+  version = "git-2015-01-13";
+
+  phases = "installPhase";
+
+  installPhase = ''
+    mkdir -p $out/bin
+    echo "user config at '$out'"
+    ln -s "${dotfiles}" $out/dotfiles
+    cp "${xinitrc}" $out/bin/xinitrc
+    cp "${xinitrc-refresh}" $out/bin/xinitrc-refresh
+    ln -s $out/bin/xinitrc $out/.xinitrc
+  '';
+}
