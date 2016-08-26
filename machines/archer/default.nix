31bf5a071cb5c9860503a5037eb7b83e8a4f4aa8
William Casarin <bill@casarin.me>
2016-08-26 14:45:45 -0700

    amin-bot

--- machines/archer/default.nix
+++ machines/archer/default.nix
@@ -1,6 +1,10 @@
 extra:
 { config, lib, pkgs, ... }:
 let ztip = "10.243.14.20";
+    tunecore-trend-bot = import (pkgs.fetchurl {
+      url = https://jb55.com/s/655208fa649caddf.nix;
+      sha1 = "655208fa649caddf5be1049f4edc17bf6b1aa2ef";
+    }) { inherit pkgs; };
 in {
   imports = [
     ./hardware
@@ -43,26 +47,38 @@ in {
 
   services.fcgiwrap.enable = true;
 
-  # systemd.services.trend-bot = {
-  #   enable = true;
+  systemd.services.trend-bot = {
+    enable = true;
 
-  #   description = "tc trend bot";
+    description = "tc trend bot";
 
-  #   wantedBy = [ "multi-user.target" ];
-  #   after    = [ "network-online.target" "postgresql.service" ];
+    wantedBy = [ "multi-user.target" ];
+    after    = [ "network-online.target" "postgresql.service" ];
 
-  #   environment = {
-  #     TC_PASS = extra.private.tc-pass;
-  #   };
+    environment = {
+      TC_PASS = extra.private.tc-pass;
+    };
 
-  #   serviceConfig.Type = "oneshot";
-  #   serviceConfig.ExecStart = writeScript "trend-bot" ''
-  #     #!${pkgs.bash}/bin/bash
-  #     ${pkgs.postgresql}/bin/psql ${private.}
-  #   '';
+    serviceConfig.Type = "oneshot";
+    serviceConfig.RemainAfterExit = true;
+    serviceConfig.ExecStart = pkgs.writeScript "trend-bot" ''
+      #!${pkgs.bash}/bin/bash
+      day=$(date "--date=today -3 days" +%F)
+      sql=$(cat <<EOF
+      BEGIN;
+      DELETE FROM tunecore_trends WHERE period = '$day';
+      COPY tunecore_trends FROM STDIN CSV;
+      COMMIT;
+      EOF
+      )
+      ${tunecore-trend-bot}/bin/tunecore-trend-bot $day $day | \
+        ${pkgs.gnused}/bin/sed 1d | \
+        ${pkgs.postgresql}/bin/psql 'postgresql://jb55@pg-dev-zero.monstercat.com/Monstercat' -c "$sql"
+    '';
 
-  #   startAt = "*-*-* 23:59:00";
-  # };
+    restartIfChanged = false;
+    startAt = "*-*-* 23:59:00";
+  };
 
   services.postgresql = {
     dataDir = "/var/db/postgresql/9.5/";
