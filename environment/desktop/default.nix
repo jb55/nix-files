b83727048d57e4b30ce46d1833cf0c9ce09e7c07
William Casarin <bill@casarin.me>
2016-08-25 23:39:28 -0700

    GTK_IM_MODULE=xim

--- environment/desktop/default.nix
+++ environment/desktop/default.nix
@@ -15,12 +15,13 @@ let gtk2rc = pkgs.writeText "gtk2rc" ''
 in {
   environment.variables = {
     GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache";
+    GTK2_RC_FILES = "${gtk2rc}:${theme.package}/share/themes/${theme.name}/gtk-2.0/gtkrc:$GTK2_RC_FILES";
     GTK_DATA_PREFIX = "${theme.package}";
     GTK_EXEC_PREFIX = "${theme.package}";
+    GTK_IM_MODULE = "xim";
     GTK_PATH = "${theme.package}:${pkgs.gtk3.out}";
     GTK_THEME = "${theme.name}";
     QT_STYLE_OVERRIDE = "GTK+";
-    GTK2_RC_FILES = "${gtk2rc}:${theme.package}/share/themes/${theme.name}/gtk-2.0/gtkrc:$GTK2_RC_FILES";
   };
 
   environment.systemPackages = with pkgs; [
