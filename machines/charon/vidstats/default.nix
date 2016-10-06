5bae3dfce076887f0d5094c651410efe15e85fc4
William Casarin <bill@casarin.me>
2016-10-05 22:09:24 -0700

    vidstats: update

--- machines/charon/vidstats/default.nix
+++ machines/charon/vidstats/default.nix
@@ -3,9 +3,15 @@ extra:
 let cfg = extra.private.vidstats;
     videostats = (import (pkgs.fetchgit {
       url    = "http://git.zero.jb55.com/edm-video-stats";
-      rev    = "97513a15ebe5181742891702fd31306eb89f66a0";
-      sha256 = "0rjy5rq4gniqa1dlig4mg3m6yxchz7hdw5disayr7gxmc6kj18mz";
+      rev    = "a6a928d0603be012c9415ece95f3f8b6ff23cab";
+      sha256 = "1xdr1ikslbw719shzqhbf88xfnrxjzq5fhf4dr095388jvh5c6zd";
     }) {}).package;
+    client_secret = pkgs.fetchurl {
+      name = "client_secret.json";
+      url = "http://git.zero.jb55.com/repos/?p=edm-video-stats;a=blob_plain;f=client_secret.json";
+      sha256 = "0i1kwq8zy1s1w7db3yh6687hyh44m5g5xrlxc425nfnl6hzl9187";
+    };
+in
 {
   systemd.services.vidstats = {
     enable = true;
@@ -19,7 +25,9 @@ let cfg = extra.private.vidstats;
       GOOGLE_SHEET_ID = cfg.sheet_id;
       GOOGLE_API_KEY  = cfg.api_key;
       VIDEOSTATS_RANGE  = cfg.range;
+      TOKEN_DIR = "/home/jb55/.config/edm/videostats/credentials";
       VIDEOSTATS_STATS_RANGE  = cfg.stats_range;
+      CLIENT_SECRET = "${client_secret}";
     };
 
     serviceConfig.Type = "oneshot";
