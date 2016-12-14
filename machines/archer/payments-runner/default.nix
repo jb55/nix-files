8b14fbabdc8bdddb810a23535c2bc1417189bcaa
William Casarin <bill@casarin.me>
2016-12-14 14:44:16 -0800

    fix

--- machines/archer/payments-runner/default.nix
+++ machines/archer/payments-runner/default.nix
@@ -2,7 +2,7 @@ extra:
 { config, lib, pkgs, ... }:
 let
   monstercatpkgs = import <monstercatpkgs> {};
-  payments-processor = monstercatpkgs.payments-processor
+  payments-processor = monstercatpkgs.payments-processor;
   payment-scripts = monstercatpkgs.payment-scripts;
 in
 {
