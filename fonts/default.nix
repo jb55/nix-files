33c88914d926c10e8b25478738810d0ac512cfd6
William Casarin <bill@casarin.me>
2017-02-25 08:11:20 -0800

    fonts: mkfont

--- fonts/default.nix
+++ fonts/default.nix
@@ -1,4 +1,22 @@
 { config, lib, pkgs, ... }:
+let mkfont = n: lesrc:
+               pkgs.stdenv.mkDerivation rec {
+                 name = "${n}-${version}";
+                 src = pkgs.fetchurl lesrc;
+                 version = "1.0";
+                 phases = ["installPhase"];
+
+                 installPhase = ''
+                   mkdir -p $out/share/fonts/${n}
+                   cp -v ${src} $out/share/fonts/${n}
+                 '';
+               };
+    aldrich =
+      mkfont "aldrich" {
+        url = "https://jb55.com/s/bef303d9e370f941.ttf";
+        sha256 = "ecc2fbf1117eed2d0b1bf32ee8624077577d568f1c785699353416b67b519227";
+      };
+in
 {
   fonts = {
     enableFontDir = true;
@@ -8,6 +26,7 @@
     fontconfig.defaultFonts.monospace  = [ "Noto Mono" ];
     fontconfig.defaultFonts.sansSerif  = [ "Noto Sans" ];
     fonts = with pkgs; [
+      aldrich
       corefonts
       inconsolata
       ubuntu_font_family
