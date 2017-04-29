2d610ee54964d2352f1f284d9044211285c03b46
William Casarin <jb55@jb55.com>
2017-04-29 08:02:48 -0700

    transmission: require openvpn

--- machines/monad/networking/default.nix
+++ machines/monad/networking/default.nix
@@ -133,6 +133,7 @@ in
     port = 14325;
   };
 
+  systemd.services.transmission.requires = [ "openvpn-pia.service" ];
   systemd.services.transmission.serviceConfig.User = lib.mkForce "root";
   systemd.services.transmission.serviceConfig.ExecStart = lib.mkForce (
     writeBash "start-transmission-under-vpn" ''
