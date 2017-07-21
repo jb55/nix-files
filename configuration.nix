f268e0055eb54938df167e587c32e36c61169867
William Casarin <jb55@jb55.com>
2017-07-21 10:48:02 -0700

    fixup caches

--- configuration.nix
+++ configuration.nix
@@ -16,9 +16,7 @@ let machine = "quiver";
       git-server = import ./misc/git-server.nix;
       util       = import ./misc/util.nix { inherit pkgs; };
     };
-    # caches = if machine == "archer"
-    #            then []
-    #            else [ "http://cache.nixos.org" ];
+    caches = [ "http://nixcache.monstercat.com" "https://cache.nixos.org" ];
     zsh = "${pkgs.zsh}/bin/zsh";
     nixpkgsConfig = import ./nixpkgs/config.nix;
     home = "/home/jb55";
@@ -77,9 +75,10 @@ in {
 
   nixpkgs.config = nixpkgsConfig;
 
-  #nix.binaryCaches = caches;
+  nix.binaryCaches = caches;
   nix.useSandbox = true;
-  # nix.trustedBinaryCaches = caches;
+  nix.trustedBinaryCaches = caches;
+  nix.requireSignedBinaryCaches = false;
 
   virtualisation.docker.enable = false;
 
