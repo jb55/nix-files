9740174330b3f9b2d09ccf7b233a3657dba291d6
William Casarin <jb55@jb55.com>
2017-08-29 00:17:11 -0700

    msmtp: dont use queue for now

--- misc/msmtp/default.nix
+++ misc/msmtp/default.nix
@@ -4,7 +4,7 @@ extra:
   services.mail.sendmailSetuidWrapper = {
     program = "sendmail";
     source = extra.util.writeBash "sendmail" ''
-      ${pkgs.msmtp}/bin/msmtpq --read-envelope-from -C /home/jb55/.msmtprc -t "$@"
+      ${pkgs.msmtp}/bin/msmtp --read-envelope-from -C /home/jb55/.msmtprc -t "$@"
     '';
     setuid = false;
     setgid = false;
