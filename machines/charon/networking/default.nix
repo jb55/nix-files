d68c58cc60d3067d150ff2350796731044506811
William Casarin <jb55@jb55.com>
2017-09-14 14:58:26 -0700

    prosody again fine

--- machines/charon/networking/default.nix
+++ machines/charon/networking/default.nix
@@ -5,7 +5,7 @@ let
   '';
 in
 {
-  networking.firewall.allowedTCPPorts = [ 22 443 80 12566 12788 ];
+  networking.firewall.allowedTCPPorts = [ 22 443 80 12566 12788 5222 5269  ];
   networking.firewall.trustedInterfaces = ["zt0"];
 
   networking.firewall.extraCommands = ''
