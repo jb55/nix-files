d05e2ccee1245b4f01fe548eb499647492cf887d
William Casarin <jb55@jb55.com>
2017-10-27 09:45:40 -0700

    tunecore-bots: update

--- machines/archer/tunecore-gaming-sales-bot/default.nix
+++ machines/archer/tunecore-gaming-sales-bot/default.nix
@@ -1,12 +1,13 @@
 extra:
 { config, lib, pkgs, ... }:
 {
-  systemd.user.services.tunecore-sales-bot = {
-    description = "tunecore sales bot";
+  systemd.user.services.tunecore-gaming-sales-bot = {
+    description = "tunecore-gaming sales bot";
 
     environment = {
-      TUNECORE_USER = extra.private.tc-user;
-      TUNECORE_PASS = extra.private.tc-pass;
+      TUNECORE_TYPE = "gaming";
+      TUNECORE_USER = extra.private.tc-gaming-user;
+      TUNECORE_PASS = extra.private.tc-gaming-pass;
       AWS_ACCESS_KEY_ID = extra.private.aws_access_key;
       AWS_SECRET_ACCESS_KEY = extra.private.aws_secret_key;
       PGDATABASE = extra.private.pgdatabase;
@@ -17,8 +18,8 @@ extra:
     serviceConfig.ExecStart = "${extra.import-scripts}/bin/tunecore-sales-bot";
     unitConfig.OnFailure = "notify-failed@%n.service";
 
-    # every saturday
-    startAt = "Tue *-*-1..7 6:30:00";
+    # first tuesday on each month
+    startAt = "Tue *-*-1..7 8:30:00";
   };
 }
 
