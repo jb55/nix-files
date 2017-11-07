a2c315716d7e686446fd7f0bc6cf20371e686cf7
William Casarin <jb55@jb55.com>
2017-11-07 12:31:14 -0800

    itunes-bots: fix error

--- machines/archer/itunes-bots/default.nix
+++ machines/archer/itunes-bots/default.nix
@@ -1,7 +1,8 @@
+extra:
 { config, lib, pkgs, ... }:
 let util = extra.util;
     import-scripts = extra.import-scripts;
-    countries = fetchurl {
+    countries = pkgs.fetchurl {
       url    = "https://jb55.com/s/8536f14537bbb417.csv";
       sha256 = "9c31690e31f5a26b12bc5a16d3a1508a06ac1d842e4a129868bc7aaf33358ab5";
     };
