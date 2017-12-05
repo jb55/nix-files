cb581e3ad88f3dcb622db402632d58737b98069c
William Casarin <jb55@jb55.com>
2017-12-05 12:09:07 -0800

    quiver: make sure we still have nixos.org cache

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,7 @@ let machine = "quiver";
       util       = import ./misc/util.nix { inherit pkgs; };
       private    = import ./private.nix;
     };
-    caches = [ "http://nixcache.monstercat.com" ];
+    caches = [ "https://cache.nixos.org" "http://nixcache.monstercat.com" ];
     zsh = "${pkgs.zsh}/bin/zsh";
     composeKey = if machine == "quiver" then "ralt" else "rwin";
     nixpkgsConfig = import ./nixpkgs/config.nix;
