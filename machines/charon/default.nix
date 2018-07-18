48e9ac978f814fde4c2d593b7a69be5a6dff5fa9
William Casarin <bill@casarin.me>
2018-07-17 22:29:35 -0700

    update

--- machines/charon/default.nix
+++ machines/charon/default.nix
@@ -40,6 +40,12 @@ in
     webroot = "/var/www/challenges";
     group = "jb55cert";
     allowKeysForGroup = true;
+    postRun = "systemctl restart prosody";
+    email = myemail;
+  };
+
+  security.acme.certs."coretto.io" = {
+    webroot = "/var/www/challenges";
     email = myemail;
   };
 
@@ -74,18 +80,18 @@ in
     sieves = builtins.readFile ./dovecot/filters.sieve;
   };
 
+  users.extraUsers.prosody.extraGroups = [ "jb55cert" ];
   services.prosody.enable = true;
   services.prosody.admins = [ "jb55@jb55.com" ];
   services.prosody.allowRegistration = false;
   services.prosody.extraModules = [
-    "cloud_notify"
-    "smacks"
-    "carbons"
-    "http_upload"
+    # "cloud_notify"
+    # "smacks"
+    # "carbons"
+    # "http_upload"
   ];
   services.prosody.extraConfig = ''
     c2s_require_encryption = true
-    http_upload_path = "/www/jb55/xmpp-upload"
   '';
   services.prosody.ssl = {
     cert = "${config.security.acme.directory}/jb55.com/fullchain.pem";
@@ -101,7 +107,8 @@ in
   };
 
   services.postgresql = {
-    dataDir = "/var/db/postgresql/9.5/";
+    dataDir = "/var/db/postgresql/9.5";
+    #package = pkgs.postgresql95;
     enable = true;
     authentication = ''
       # type db  user address        method
@@ -122,52 +129,52 @@ in
     serviceConfig.ExecStart = "${npmrepo}/bin/npm-repo-proxy";
   };
 
-  systemd.user.services.rss2email = {
-    description = "run rss2email";
-    path = with pkgs; [ rss2email ];
-    wantedBy = [ "default.target" ];
-    serviceConfig.ExecStart = "${pkgs.rss2email}/bin/r2e run";
-  };
+  # systemd.user.services.rss2email = {
+  #   description = "run rss2email";
+  #   path = with pkgs; [ rss2email ];
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig.ExecStart = "${pkgs.rss2email}/bin/r2e run";
+  # };
 
-  systemd.user.services.backup-rss2email = {
-    description = "backup rss2email";
-    wantedBy = [ "default.target" ];
-    serviceConfig.ExecStart = pkgs.writeScript "backup-rss2email" ''
-      #!${pkgs.bash}/bin/bash
-      BACKUP_DIR=/home/jb55/backups/rss2email
-      cp /home/jb55/.config/rss2email.cfg $BACKUP_DIR
-      cp /home/jb55/.local/share/rss2email.json $BACKUP_DIR
-      cd $BACKUP_DIR
-      ${pkgs.git}/bin/git add -u
-      ${pkgs.git}/bin/git commit -m "bump"
-      ${pkgs.git}/bin/git push
-    '';
-  };
+  # systemd.user.services.backup-rss2email = {
+  #   description = "backup rss2email";
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig.ExecStart = pkgs.writeScript "backup-rss2email" ''
+  #     #!${pkgs.bash}/bin/bash
+  #     BACKUP_DIR=/home/jb55/backups/rss2email
+  #     cp /home/jb55/.config/rss2email.cfg $BACKUP_DIR
+  #     cp /home/jb55/.local/share/rss2email.json $BACKUP_DIR
+  #     cd $BACKUP_DIR
+  #     ${pkgs.git}/bin/git add -u
+  #     ${pkgs.git}/bin/git commit -m "bump"
+  #     ${pkgs.git}/bin/git push
+  #   '';
+  # };
 
-  systemd.user.timers.backup-rss2email = {
-    wantedBy = [ "timers.target" ];
-    timerConfig.OnCalendar = "daily";
-  };
+  # systemd.user.timers.backup-rss2email = {
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig.OnCalendar = "daily";
+  # };
 
-  systemd.user.timers.rss2email = {
-    wantedBy = [ "timers.target" ];
-    timerConfig.OnCalendar = "hourly";
-  };
+  # systemd.user.timers.rss2email = {
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig.OnCalendar = "hourly";
+  # };
 
-  systemd.services.hearpress = {
-    description = "Hearpress server";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "postgresql.service" ];
+  # systemd.services.hearpress = {
+  #   description = "Hearpress server";
+  #   wantedBy = [ "multi-user.target" ];
+  #   after = [ "postgresql.service" ];
 
-    environment = {
-      PG_CS = "postgresql://jb55@localhost/hearpress";
-      AWS_ACCESS_KEY_ID = extra.private.aws.access_key;
-      AWS_SECRET_ACCESS_KEY = extra.private.aws.secret_key;
-    };
+  #   environment = {
+  #     PG_CS = "postgresql://jb55@localhost/hearpress";
+  #     AWS_ACCESS_KEY_ID = extra.private.aws.access_key;
+  #     AWS_SECRET_ACCESS_KEY = extra.private.aws.secret_key;
+  #   };
 
-    serviceConfig.Type = "simple";
-    serviceConfig.ExecStart = "${hearpress}/bin/hearpressd";
-  };
+  #   serviceConfig.Type = "simple";
+  #   serviceConfig.ExecStart = "${hearpress}/bin/hearpressd";
+  # };
 
   services.dnsmasq.enable = false;
   services.dnsmasq.servers = ["8.8.8.8" "8.8.4.4"];
--- machines/charon/networking/default.nix
+++ machines/charon/networking/default.nix
@@ -7,6 +7,12 @@ in
 {
   networking.firewall.allowedTCPPorts = [ 22 443 80 12566 12788 5222 5269  ];
   networking.firewall.trustedInterfaces = ["zt0"];
+  networking.domain = "jb55.com";
+  networking.search = [ "jb55.com" ];
+  networking.extraHosts = ''
+    127.0.0.1 jb55.com
+    ::1 jb55.com
+  '';
 
   networking.firewall.extraCommands = ''
     ${openTCP "zt0" 993}
