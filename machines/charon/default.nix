5988205a1440f1dc2a4b8047c8dbc5d5f55fe73c
William Casarin <bill@casarin.me>
2018-09-18 18:27:58 -0700

    Revert "remove rss2email comments"
    This reverts commit d5d47f0e01cf17df2312dde2eb8ac8827e46555a.

--- machines/charon/default.nix
+++ machines/charon/default.nix
@@ -237,6 +237,52 @@ in
     serviceConfig.ExecStart = "${npmrepo}/bin/npm-repo-proxy";
   };
 
+  # systemd.user.services.rss2email = {
+  #   description = "run rss2email";
+  #   path = with pkgs; [ rss2email ];
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig.ExecStart = "${pkgs.rss2email}/bin/r2e run";
+  # };
+
+  # systemd.user.services.backup-rss2email = {
+  #   description = "backup rss2email";
+  #   wantedBy = [ "default.target" ];
+  #   serviceConfig.ExecStart = pkgs.writeScript "backup-rss2email" ''
+  #     #!${pkgs.bash}/bin/bash
+  #     BACKUP_DIR=/home/jb55/backups/rss2email
+  #     cp /home/jb55/.config/rss2email.cfg $BACKUP_DIR
+  #     cp /home/jb55/.local/share/rss2email.json $BACKUP_DIR
+  #     cd $BACKUP_DIR
+  #     ${pkgs.git}/bin/git add -u
+  #     ${pkgs.git}/bin/git commit -m "bump"
+  #     ${pkgs.git}/bin/git push
+  #   '';
+  # };
+
+  # systemd.user.timers.backup-rss2email = {
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig.OnCalendar = "daily";
+  # };
+
+  # systemd.user.timers.rss2email = {
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig.OnCalendar = "hourly";
+  # };
+
+  # systemd.services.hearpress = {
+  #   description = "Hearpress server";
+  #   wantedBy = [ "multi-user.target" ];
+  #   after = [ "postgresql.service" ];
+
+  #   environment = {
+  #     PG_CS = "postgresql://jb55@localhost/hearpress";
+  #     AWS_ACCESS_KEY_ID = extra.private.aws.access_key;
+  #     AWS_SECRET_ACCESS_KEY = extra.private.aws.secret_key;
+  #   };
+
+  #   serviceConfig.Type = "simple";
+  #   serviceConfig.ExecStart = "${hearpress}/bin/hearpressd";
+  # };
 
   services.dnsmasq.enable = false;
   services.dnsmasq.servers = ["8.8.8.8" "8.8.4.4"];
