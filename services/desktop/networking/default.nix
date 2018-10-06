377f731c69ed3920bc5a64b66e0e33ff212b655a
William Casarin <jb55@jb55.com>
2018-10-05 22:28:09 -0700

    fix vpn

--- services/desktop/networking/default.nix
+++ services/desktop/networking/default.nix
@@ -50,7 +50,7 @@ in
         client
         dev tun
         proto udp
-        remote 185.153.179.9 1194
+        remote 172.83.40.224 1194
         resolv-retry infinite
         remote-random
         nobind
@@ -63,14 +63,15 @@ in
         ping-restart 0
         ping-timer-rem
         reneg-sec 0
-        explicit-exit-notify 3
+
+        auth-user-pass ${vpn.credfile}
+        route-noexec
+        route-up ${vpn.routeup}
 
         remote-cert-tls server
 
-        #mute 10000
-        auth-user-pass ${vpn.credfile}
-
-        comp-lzo
+        # Compress-lzo is not compactible with 2.3 clients so using deprecated comp-lzo
+        comp-lzo no
         verb 3
         pull
         fast-io
@@ -79,58 +80,59 @@ in
 
         <ca>
         -----BEGIN CERTIFICATE-----
-        MIIExzCCA6+gAwIBAgIJAOPEhSAzJHDIMA0GCSqGSIb3DQEBCwUAMIGdMQswCQYD
-        VQQGEwJQQTELMAkGA1UECBMCUEExDzANBgNVBAcTBlBhbmFtYTEQMA4GA1UEChMH
-        Tm9yZFZQTjEQMA4GA1UECxMHTm9yZFZQTjEZMBcGA1UEAxMQY2E5NC5ub3JkdnBu
-        LmNvbTEQMA4GA1UEKRMHTm9yZFZQTjEfMB0GCSqGSIb3DQEJARYQY2VydEBub3Jk
-        dnBuLmNvbTAeFw0xNzA0MjAxMTE4NDdaFw0yNzA0MTgxMTE4NDdaMIGdMQswCQYD
-        VQQGEwJQQTELMAkGA1UECBMCUEExDzANBgNVBAcTBlBhbmFtYTEQMA4GA1UEChMH
-        Tm9yZFZQTjEQMA4GA1UECxMHTm9yZFZQTjEZMBcGA1UEAxMQY2E5NC5ub3JkdnBu
-        LmNvbTEQMA4GA1UEKRMHTm9yZFZQTjEfMB0GCSqGSIb3DQEJARYQY2VydEBub3Jk
-        dnBuLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKpFRsSPZsq6
-        EQBIRZeHL6xxOZY2z5P2yraIUU39+J6R5GTwcUbOSd9ufxRfE0/CszT8QP5f4qyE
-        mxYhMxswN+XfMN8Mmb2zNU6PWmPFlYgzkwPedmd2yHHkBiS3LlA1S0OzTbd3pO9B
-        K9XV72LMQpyhcU+0SI4xlG6Q3AhyotVNIZfcpwqa3iODV3Ho/xKwRc3MTMDAP+ze
-        vxR7QyBIDqmUY0wLf94vL8YlfwoVF0DvJ7k0fXX4anUBt6XVzKtqiV9shbPHixGI
-        CyBwsEUFTuQNrNVw9mRF3KqeoEAWTQaNZClswWpLT8OZyg1jzmVM+Uk3ihkJL7kn
-        q0kL3qYn0JkCAwEAAaOCAQYwggECMB0GA1UdDgQWBBQu53WFpw5k+Q1V7g+rQT5w
-        ehBzKDCB0gYDVR0jBIHKMIHHgBQu53WFpw5k+Q1V7g+rQT5wehBzKKGBo6SBoDCB
-        nTELMAkGA1UEBhMCUEExCzAJBgNVBAgTAlBBMQ8wDQYDVQQHEwZQYW5hbWExEDAO
-        BgNVBAoTB05vcmRWUE4xEDAOBgNVBAsTB05vcmRWUE4xGTAXBgNVBAMTEGNhOTQu
-        bm9yZHZwbi5jb20xEDAOBgNVBCkTB05vcmRWUE4xHzAdBgkqhkiG9w0BCQEWEGNl
-        cnRAbm9yZHZwbi5jb22CCQDjxIUgMyRwyDAMBgNVHRMEBTADAQH/MA0GCSqGSIb3
-        DQEBCwUAA4IBAQCQ7pdz6SH7IOcp5zGl63EhImhyvy6aD3c0J8XycGyh/DONFrj2
-        ApdKuahny5ZfoZV+zP6NvaU3auoNGAGsRlOoCeY1edmevsCysC2umJKMtNBRNMTX
-        51CTCDuM7jXZ6oNhy0F1XEeOFT+t9WpeNzab5vNuPclAR57WZzbwGYIHUV1wQyWj
-        MTYfwcv0E9uNwF0r0y9NV5k63EyOvWRKjGy6YI7Tv1wtnyD+B10VaXBuRKge46fV
-        tB83AkJwF00C+Dyy1BL4X1UmUAlusbz/YZnxq6HRJmrEY6+7p4+fOUbHCtM7Xtqf
-        rjfKjYoPeoUnKSYDwx0GepuaSNVWtqEgHYyE
+        MIIFCjCCAvKgAwIBAgIBATANBgkqhkiG9w0BAQ0FADA5MQswCQYDVQQGEwJQQTEQ
+        MA4GA1UEChMHTm9yZFZQTjEYMBYGA1UEAxMPTm9yZFZQTiBSb290IENBMB4XDTE2
+        MDEwMTAwMDAwMFoXDTM1MTIzMTIzNTk1OVowOTELMAkGA1UEBhMCUEExEDAOBgNV
+        BAoTB05vcmRWUE4xGDAWBgNVBAMTD05vcmRWUE4gUm9vdCBDQTCCAiIwDQYJKoZI
+        hvcNAQEBBQADggIPADCCAgoCggIBAMkr/BYhyo0F2upsIMXwC6QvkZps3NN2/eQF
+        kfQIS1gql0aejsKsEnmY0Kaon8uZCTXPsRH1gQNgg5D2gixdd1mJUvV3dE3y9FJr
+        XMoDkXdCGBodvKJyU6lcfEVF6/UxHcbBguZK9UtRHS9eJYm3rpL/5huQMCppX7kU
+        eQ8dpCwd3iKITqwd1ZudDqsWaU0vqzC2H55IyaZ/5/TnCk31Q1UP6BksbbuRcwOV
+        skEDsm6YoWDnn/IIzGOYnFJRzQH5jTz3j1QBvRIuQuBuvUkfhx1FEwhwZigrcxXu
+        MP+QgM54kezgziJUaZcOM2zF3lvrwMvXDMfNeIoJABv9ljw969xQ8czQCU5lMVmA
+        37ltv5Ec9U5hZuwk/9QO1Z+d/r6Jx0mlurS8gnCAKJgwa3kyZw6e4FZ8mYL4vpRR
+        hPdvRTWCMJkeB4yBHyhxUmTRgJHm6YR3D6hcFAc9cQcTEl/I60tMdz33G6m0O42s
+        Qt/+AR3YCY/RusWVBJB/qNS94EtNtj8iaebCQW1jHAhvGmFILVR9lzD0EzWKHkvy
+        WEjmUVRgCDd6Ne3eFRNS73gdv/C3l5boYySeu4exkEYVxVRn8DhCxs0MnkMHWFK6
+        MyzXCCn+JnWFDYPfDKHvpff/kLDobtPBf+Lbch5wQy9quY27xaj0XwLyjOltpiST
+        LWae/Q4vAgMBAAGjHTAbMAwGA1UdEwQFMAMBAf8wCwYDVR0PBAQDAgEGMA0GCSqG
+        SIb3DQEBDQUAA4ICAQC9fUL2sZPxIN2mD32VeNySTgZlCEdVmlq471o/bDMP4B8g
+        nQesFRtXY2ZCjs50Jm73B2LViL9qlREmI6vE5IC8IsRBJSV4ce1WYxyXro5rmVg/
+        k6a10rlsbK/eg//GHoJxDdXDOokLUSnxt7gk3QKpX6eCdh67p0PuWm/7WUJQxH2S
+        DxsT9vB/iZriTIEe/ILoOQF0Aqp7AgNCcLcLAmbxXQkXYCCSB35Vp06u+eTWjG0/
+        pyS5V14stGtw+fA0DJp5ZJV4eqJ5LqxMlYvEZ/qKTEdoCeaXv2QEmN6dVqjDoTAo
+        k0t5u4YRXzEVCfXAC3ocplNdtCA72wjFJcSbfif4BSC8bDACTXtnPC7nD0VndZLp
+        +RiNLeiENhk0oTC+UVdSc+n2nJOzkCK0vYu0Ads4JGIB7g8IB3z2t9ICmsWrgnhd
+        NdcOe15BincrGA8avQ1cWXsfIKEjbrnEuEk9b5jel6NfHtPKoHc9mDpRdNPISeVa
+        wDBM1mJChneHt59Nh8Gah74+TM1jBsw4fhJPvoc7Atcg740JErb904mZfkIEmojC
+        VPhBHVQ9LHBAdM8qFI2kRK0IynOmAZhexlP/aT/kpEsEPyaZQlnBn3An1CRz8h0S
+        PApL8PytggYKeQmRhl499+6jLxcZ2IegLfqq41dzIjwHwTMplg+1pKIOVojpWA==
         -----END CERTIFICATE-----
         </ca>
         key-direction 1
         <tls-auth>
+        #
+        # 2048 bit OpenVPN static key
+        #
         -----BEGIN OpenVPN Static key V1-----
-        da7b64ee7bb880f0783781e2bcb3d624
-        76e55ecb0c525448c0844a0d03b66636
-        8568db261ce272cdb7c65c623fdb4088
-        82e5c608c28c3c36655c98cf7ebb5770
-        1a987f7a689af9f86a283761fbbe1304
-        8372cbb215f64054e48157059b71c167
-        f50d8e9d1f0d9ae1902d51b7fae0d4b6
-        1936398446999b4b5e51f374e66d77c6
-        205cb63372b2ef820e3d415547317726
-        d70bd6ea1e67ea35055f1026197bbed4
-        7a3211703da08b851a96d6b02fc05b0d
-        bb76eeabaa62969b4b5025dbe68580c8
-        df950ede15f5a7a845aa97f4a43da8b7
-        0810a3c87af1ca8c95327c72b80871d1
-        fa31da4e31fffb7119b99c7a847f9c59
-        8480ea742b8a7b91bf521ec0987ba03d
+        e685bdaf659a25a200e2b9e39e51ff03
+        0fc72cf1ce07232bd8b2be5e6c670143
+        f51e937e670eee09d4f2ea5a6e4e6996
+        5db852c275351b86fc4ca892d78ae002
+        d6f70d029bd79c4d1c26cf14e9588033
+        cf639f8a74809f29f72b9d58f9b8f5fe
+        fc7938eade40e9fed6cb92184abb2cc1
+        0eb1a296df243b251df0643d53724cdb
+        5a92a1d6cb817804c4a9319b57d53be5
+        80815bcfcb2df55018cc83fc43bc7ff8
+        2d51f9b88364776ee9d12fc85cc7ea5b
+        9741c4f598c485316db066d52db4540e
+        212e1518a9bd4828219e24b20d88f598
+        a196c9de96012090e333519ae18d3509
+        9427e7b372d348d352dc4c85e18cd4b9
+        3f8a56ddb2e64eb67adfc9b337157ff4
         -----END OpenVPN Static key V1-----
         </tls-auth>
-
-        route-noexec
-        route-up ${vpn.routeup}
       '';
 
       up = ''
