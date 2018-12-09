5040248421aa01f3da1cc78405dced998ef50115
William Casarin <jb55@jb55.com>
2018-12-09 13:47:00 -0800

    move bitcoin stuff to its own file

--- machines/monad/default.nix
+++ machines/monad/default.nix
@@ -24,6 +24,7 @@ in
 {
   imports = [
     ./hardware
+    ./bitcoin.nix
     (import ../../misc/msmtp extra)
     (import ./networking extra)
     (import ../../misc/imap-notifier extra)
@@ -43,82 +44,6 @@ in
   users.extraGroups.transmission.members = [ "nginx" "jb55" ];
 
   programs.mosh.enable = true;
-
-  services.spruned = {
-    enable = false;
-    dataDir = "/zbig/spruned";
-    network = "mainnet";
-    extraArguments = "--debug";
-  };
-
-  services.bitcoind.networks = {
-    testnet = {
-      testnet = true;
-      dataDir = "/var/lib/bitcoin-testnet";
-      extraConfig = ''
-        txindex=1
-        rpcuser=rpcuser
-        rpcpassword=rpcpass
-        rpcallowip=1
-        rpcbind=172.24.242.111
-        rpcbind=10.147.20.220
-        rpcbind=127.0.0.1
-        rpcport=6533
-      '';
-    };
-
-    mainnet = {
-      dataDir = "/var/lib/bitcoin";
-      extraConfig = ''
-        txindex=1
-        rpcuser=rpcuser
-        rpcpassword=rpcpass
-        rpcallowip=1
-        rpcbind=172.24.242.111
-        rpcbind=10.147.20.220
-        rpcbind=127.0.0.1
-        rpcport=6532
-      '';
-    };
-  };
-
-  services.clightning.networks = {
-    testnet = {
-      dataDir = "/home/jb55/.lightning";
-
-      config = ''
-        fee-per-satoshi=9000
-        bitcoin-rpcuser=rpcuser
-        bitcoin-rpcpassword=rpcpass
-        bitcoin-rpcconnect=127.0.0.1
-        bitcoin-rpcport=6533
-        bind-addr=0.0.0.0:9736
-        announce-addr=24.84.152.187:9736
-        network=testnet
-        log-level=debug
-        alias=@jb55
-        rgb=ff0000
-      '';
-    };
-
-    mainnet = {
-      dataDir = "/home/jb55/.lightning-bitcoin";
-
-      config = ''
-        bitcoin-rpcuser=rpcuser
-        bitcoin-rpcpassword=rpcpass
-        bitcoin-rpcconnect=127.0.0.1
-        bitcoin-rpcport=6532
-        fee-per-satoshi=9000
-        bind-addr=0.0.0.0:9735
-        announce-addr=24.84.152.187:9735
-        network=bitcoin
-        log-level=debug
-        alias=@jb55
-        rgb=ff0000
-      '';
-    };
-  };
 
   documentation.nixos.enable = false;
 
