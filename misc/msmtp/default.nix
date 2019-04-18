0d2a09d4a507aee5e6e477fd582f3b899d30ce6f
William Casarin <jb55@jb55.com>
2019-04-18 10:04:46 -0700

    mail: fix sendmail double-sends

--- misc/msmtp/default.nix
+++ misc/msmtp/default.nix
@@ -1,12 +1,4 @@
 extra:
 { config, lib, pkgs, ... }:
 {
-  services.mail.sendmailSetuidWrapper = {
-    program = "sendmail";
-    source = extra.util.writeBash "sendmail" ''
-      ${pkgs.msmtp}/bin/msmtp --read-envelope-from -C /home/jb55/.msmtprc -t "$@"
-    '';
-    setuid = false;
-    setgid = false;
-  };
 }
