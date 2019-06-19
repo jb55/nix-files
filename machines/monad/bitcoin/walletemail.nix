3fd1280dfb45fe044ee17e1c9cc99d53c83b5e46
William Casarin <jb55@jb55.com>
2019-06-19 10:44:49 -0700

    walletemail fixups

--- machines/monad/bitcoin/walletemail.nix
+++ machines/monad/bitcoin/walletemail.nix
@@ -14,17 +14,20 @@ subject="Wallet notification"
 keys="-r 0x8860420C3C135662EABEADF96342E010C44A6337 -r 0x5B2B1E4F62216BC74362AC61D4FBA2FC4535A2A9 -r 0xE02D3FD4EB4585A63531C1D0E1BFCB90A1FF7A1C"
 
 tx="$(${bcli} -rpcwallet=$wallet gettransaction "$txid" true)"
+address="$(${pkgs.jq}/bin/jq -r '.details[0].address' <<<"$tx")"
+details="$(${pkgs.jq}/bin/jq -r '.details[] | [.address, .category, .amount, .label] | @csv' <<<"$tx")"
+keypath="$(${bcli} -rpcwallet=$wallet getaddressinfo "$address" | ${pkgs.jq}/bin/jq -r .hdkeypath)"
 
-time="$(date -d @$(${pkgs.jq}/bin/jq -r .blocktime <<<"$tx"))"
+amount="$(${pkgs.jq}/bin/jq -r '.amount' <<<"$tx")"
+confs="$(${pkgs.jq}/bin/jq -r '.confirmations' <<<"$tx")"
+
+time="$(date -d @$(${pkgs.jq}/bin/jq -r '.time' <<<"$tx"))"
+received="$(date -d @$(${pkgs.jq}/bin/jq -r '.timereceived' <<<"$tx"))"
 
 export GNUPGHOME=/zbig/bitcoin/gpg
 
-details="$(${pkgs.jq}/bin/jq -r '["amount","address","category"],(.details[] | [.amount, .address, .category]) | @tsv' <<<"$tx" | ${pkgs.utillinux}/bin/column -t -s $'\t')"
-
-keypath="$(${pkgs.jq}/bin/jq -r .hdkeypath <<<"$tx")"
-
-msg="$(printf "txid: %s\n\nwallet: %s\n\ndate: %s\n\nkeypath: %s\n\n\ndetails: %s\n" \
-              "$txid"            "$wallet" "$time"      "$keypath"     "$details")"
+msg="$(printf "txid: %s\n\naddress: %s\n\namount: %s\n\nconfirmations: %d\n\nwallet: %s\n\ntime: %s\n\nreceived: %s\n\nkeypath: %s\n\n%s\n\n\n%s" \
+              "$txid" "$address" "$amount" "$confs" "$wallet" "$time" "$received" "$keypath" "$details" "$tx" )"
 
 enctx="$(printf "Content-Type: text/plain\n\n%s\n" "$msg" | ${pkgs.gnupg}/bin/gpg --yes --always-trust --encrypt --armor $keys)"
 
