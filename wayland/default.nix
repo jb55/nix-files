96320844fa25e3032f29058c0cedf1729a1e7372
William Casarin <jb55@jb55.com>
2020-01-03 09:54:21 -0800

    wayland updates
    Signed-off-by: William Casarin <jb55@jb55.com>

--- wayland/default.nix
+++ wayland/default.nix
@@ -5,18 +5,14 @@ let
 in
 {
     nixpkgs.overlays = [ waylandOverlay ];
-    programs.sway-beta.enable = true;
-    programs.sway-beta.extraPackages = with pkgs; [
+    programs.sway.enable = true;
+    programs.sway.extraPackages = with pkgs; [
       swayidle # used for controlling idle timeouts and triggers (screen locking, etc)
       swaylock # used for locking Wayland sessions
-
-      waybar        # polybar-alike
-      i3status-rust # simpler bar written in Rust
 
       grim     # screen image capture
       slurp    # screen are selection tool
       mako     # notification daemon
-      wlstream # screen recorder
       oguri    # animated background utility
       kanshi   # dynamic display configuration helper
       redshift-wayland # patched to work with wayland gamma protocol
@@ -28,6 +24,5 @@ in
       # other compositors/window-managers
       wayfire  # 3D wayland compositor
       waybox   # An openbox clone on Wayland
-      bspwc    # Wayland compositor based on BSPWM
     ];
 }
