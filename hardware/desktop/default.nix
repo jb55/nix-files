6e89bcbf06ded4bd0183f168d0e55f786ba58223
William Casarin <jb55@jb55.com>
2020-03-25 21:28:34 -0700

    switch controller udev
    Signed-off-by: William Casarin <jb55@jb55.com>

--- hardware/desktop/default.nix
+++ hardware/desktop/default.nix
@@ -49,6 +49,12 @@ in
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="054c", ATTRS{idProduct}=="05c4", MODE="0666"
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", KERNELS=="0005:054C:05C4.*", MODE="0666"
 
+    # Nintendo Switch Pro Controller over USB hidraw
+    KERNEL=="hidraw*", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="2009", MODE="0666"
+
+    # Nintendo Switch Pro Controller over bluetooth hidraw
+    KERNEL=="hidraw*", KERNELS=="*057E:2009*", MODE="0666"
+
     # rtl-sdr
     SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="2832", MODE="0666", SYMLINK+="rtl_sdr"
 
